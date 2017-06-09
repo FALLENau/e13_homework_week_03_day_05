cea66bb351df0e2fdffae2e3fa5af59ba66bf389
FALLENau <reece.c.jones@gmail.com>
2017-06-09 13:44:32 +0100

    1hr update

---
+++ cinema/db/cinema.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS films;
+DROP TABLE IF EXISTS tickets;
+
+CREATE TABLE tickets(
+  id SERIAL4 PRIMARY KEY,
+  ticket_number INT4 REFERENCES tickets(id),
+  customer_id INT4 REFERENCES customers(id),
+  film_id INT4 REFERENCES customers(id)
+)
+
+CREATE TABLE customers(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  funds INT4
+)
+
+CREATE TABLE films(
+  id SERIAL4 PRIMARY KEY,
+  title VARCHAR(255),
+  price INT4
+)
