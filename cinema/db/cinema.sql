d715f2ada18e6a225ce2bb8fb25b70b55bae136a
FALLENau <reece.c.jones@gmail.com>
2017-06-10 11:30:30 +0100

    saturday update

--- cinema/db/cinema.sql
+++ cinema/db/cinema.sql
@@ -1,13 +1,7 @@
-DROP TABLE IF EXISTS tickets;
-DROP TABLE IF EXISTS customers;
-DROP TABLE IF EXISTS films;
+DROP TABLE IF EXISTS tickets CASCADE;
+DROP TABLE IF EXISTS customers CASCADE;
+DROP TABLE IF EXISTS films CASCADE;
 
-
-CREATE TABLE customers (
-  id SERIAL4 PRIMARY KEY,
-  name VARCHAR(255),
-  funds INT4
-);
 
 CREATE TABLE films (
   id SERIAL4 PRIMARY KEY,
@@ -15,9 +9,15 @@ CREATE TABLE films (
   price INT4
 );
 
+CREATE TABLE customers (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  funds INT4
+);
+
 CREATE TABLE tickets (
   id SERIAL4 PRIMARY KEY,
-  ticket_number INT4 REFERENCES tickets(id),
+  ticket_number INT4,
   customer_id INT4 REFERENCES customers(id),
-  film_id INT4 REFERENCES customers(id)
+  film_id INT4 REFERENCES films(id)
 );
--- cinema/db/seeds.rb
+++ cinema/db/seeds.rb
@@ -1,25 +1,37 @@
-require_relative "../models/ticket"
-require_relative "../models/customer"
-require_relative "../models/film"
+require_relative '../models/ticket'
+require_relative '../models/customer'
+require_relative '../models/film'
 
 require "pry-byebug"
 
-#Ticket.delete_all()
+Ticket.delete_all()
 #Film.delete_all()
 #Customer.delete_all()
 
+ticket1 = Ticket.new({ 'ticket_number' => 1 })
+ticket2 = Ticket.new({ 'ticket_number' => 2 })
+ticket3 = Ticket.new({ 'ticket_number' => 3 })
+
+ticket1.save()
+ticket2.save()
+ticket3.save()
+
+
 customer1 = Customer.new({ 'name' => 'Reece', 'funds' => 20 })
-customer2 = Customer.new({ 'name' => 'Eugene', 'funds' => 15 })
-customer3 = Customer.new({ 'name' => 'Ross', 'funds' => 25 })
-
+# customer2 = Customer.new({ 'name' => 'Eugene', 'funds' => 15 })
+# customer3 = Customer.new({ 'name' => 'Ross', 'funds' => 25 })
+#
 customer1.save()
-customer2.save()
-customer3.save()
-
+# customer2.save()
+# customer3.save()
+#
 film1 = Film.new({ 'title' => 'Shazam', 'price' => 12})
-film2 = Film.new({ 'title' => 'Black Adam', 'price' => 10.50})
-film3 = Film.new({ 'title' => 'Blue Bettle', 'price' => 11})
-
+# film2 = Film.new({ 'title' => 'Black Adam', 'price' => 10.50})
+# film3 = Film.new({ 'title' => 'Blue Bettle', 'price' => 11})
+#
 film1.save()
-film2.save()
-film3.save()
+# film2.save()
+# film3.save()
+
+binding.pry
+nil
