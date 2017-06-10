ac1fd0746e537b0fc47a1b2bbe1089b7a3944b6e
FALLENau <reece.c.jones@gmail.com>
2017-06-10 12:16:06 +0100

    progress id

--- cinema/db/seeds.rb
+++ cinema/db/seeds.rb
@@ -5,33 +5,48 @@ require_relative '../models/film'
 require "pry-byebug"
 
 Ticket.delete_all()
-#Film.delete_all()
-#Customer.delete_all()
+Film.delete_all()
+Customer.delete_all()
 
-ticket1 = Ticket.new({ 'ticket_number' => 1 })
-ticket2 = Ticket.new({ 'ticket_number' => 2 })
-ticket3 = Ticket.new({ 'ticket_number' => 3 })
+
+
+
+customer1 = Customer.new({ 'name' => 'Reece', 'funds' => 20 })
+customer2 = Customer.new({ 'name' => 'Eugene', 'funds' => 15 })
+customer3 = Customer.new({ 'name' => 'Ross', 'funds' => 25 })
+
+customer1.save()
+customer2.save()
+customer3.save()
+
+film1 = Film.new({ 'title' => 'Shazam', 'price' => 12})
+film2 = Film.new({ 'title' => 'Black Adam', 'price' => 10.50})
+film3 = Film.new({ 'title' => 'Blue Bettle', 'price' => 11})
+
+film1.save()
+film2.save()
+film3.save()
+
+ticket1 = Ticket.new({
+  'ticket_number' => 1,
+  'customer_id' => customer1.id,#Problem here.
+  'film_id' => film1.id
+  })
+ticket2 = Ticket.new({
+  'ticket_number' => 2,
+  'customer_id' => customer2.id,
+  'film_id' => film2.id
+})
+ticket3 = Ticket.new({
+  'ticket_number' => 3,
+  'customer_id' => customer3.id,
+  'film_id' => film3.id
+})
 
 ticket1.save()
 ticket2.save()
 ticket3.save()
 
-
-customer1 = Customer.new({ 'name' => 'Reece', 'funds' => 20 })
-# customer2 = Customer.new({ 'name' => 'Eugene', 'funds' => 15 })
-# customer3 = Customer.new({ 'name' => 'Ross', 'funds' => 25 })
-#
-customer1.save()
-# customer2.save()
-# customer3.save()
-#
-film1 = Film.new({ 'title' => 'Shazam', 'price' => 12})
-# film2 = Film.new({ 'title' => 'Black Adam', 'price' => 10.50})
-# film3 = Film.new({ 'title' => 'Blue Bettle', 'price' => 11})
-#
-film1.save()
-# film2.save()
-# film3.save()
 
 binding.pry
 nil
--- cinema/models/customer.rb
+++ cinema/models/customer.rb
@@ -2,10 +2,28 @@ require_relative("../db/sql_runner")
 
 class Customer
 
+  attr_reader :id
+  attr_accessor :name, :funds
+
   def initialize( options )
     @id = options['id'].to_i
     @name = options['name']
     @funds = options['funds'].to_i
   end
+
+  def save()
+    sql = "INSERT INTO customers (name, funds) VALUES ('#{ @name }', '#{ @funds }') RETURNING id"
+    customer = SqlRunner.run( sql ).first
+    @id = customer['id'].to_i
+  end
+
+
+
+
+  def self.delete_all()
+    sql = "DELETE FROM customers"
+    SqlRunner.run(sql)
+  end
+
 
 end
