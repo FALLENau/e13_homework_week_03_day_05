6b4124a14f531029eed1b72a9a987ac5e107fcd5
FALLENau <reece.c.jones@gmail.com>
2017-06-11 18:12:39 +0100

    homework done

--- cinema/models/ticket.rb
+++ cinema/models/ticket.rb
@@ -35,21 +35,6 @@ class Ticket
     return results
   end
 
-  # def customers()
-  #   sql = "SELECT films.* FROM films
-  #   INNER JOIN tickets
-  #   ON films.id = tickets.film_id
-  #   WHERE tickets.customer_id = #{@id}"
-  #   return Customer.map_items(sql)[0]
-  # end #Note:broken might need Customer.map_items tobe fixed.
-  # def locations()
-  #     sql = "SELECT locations.* FROM locations
-  #     INNER JOIN visits
-  #     ON locations.id = visists.location_id
-  #     WHERE visits.user_id = #{@id}"
-  #     return User.map_items(sql)
-  # end note: locations = films, visits = tickets, user = customer
-
 
   def self.all()
     sql = "SELECT * FROM tickets"
